490ce2c7b58da5da37f1279027414f76e37e23c9
ragavendra <ragavendra.bn@gmail.com>
2017-11-29 11:53:45 -0800

    assg2 handle web root

--- wiki.go
+++ wiki.go
@@ -43,7 +43,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
-			http.NotFound(w, r)
+			fn(w, r, "FrontPage")
 			return
 		}
 		fn(w, r, m[2])
@@ -59,6 +59,15 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
@@ -89,5 +98,6 @@ func main() {
     http.HandleFunc("/view/", makeHandler(viewHandler))
     http.HandleFunc("/edit/", makeHandler(editHandler))
     http.HandleFunc("/save/", makeHandler(saveHandler))
+    http.HandleFunc("/", makeHandler(viewHandler))
     http.ListenAndServe(":8080", nil)
 }
